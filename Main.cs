f3ed4224e5be26144037cf1cf71da67afd326948
SaurabhTotey <SaurabhTotey@gmail.com>
2020-03-29 12:58:21 -0600

    Documentation/formatting

--- Main.cs
+++ Main.cs
@@ -1,47 +1,61 @@
 using Godot;
 
+/**
+ * All the code for the liquid simulation
+ */
 public class Main : Control {
 
+	//The packed scene for the water particle because it is potentially instanced many times
 	private PackedScene _waterParticleScene;
 
-	private SegmentShape2D _currentPlacedBlocker;
+	//The user can place 'blockers', which are static segments that interact with the water; this is the blocker that is currently being placed
+	private SegmentShape2D _blockerSegmentBeingPlaced;
 
+	/**
+	 * Gets the WaterParticle scene as a packed scene on startup because it will be loaded many times
+	 */
 	public override void _Ready() {
 		this._waterParticleScene = ResourceLoader.Load<PackedScene>("res://WaterParticle.tscn");
 	}
 
+	/**
+	 * Handles inputs for the simulation
+	 * TODO: use keymappings
+	 */
 	public override void _Process(float delta) {
+		//Adds a water particle to the mouse's position if the left mouse button is pressed
 		if (Input.IsMouseButtonPressed(1)) {
 			var waterParticleInstance = (Node2D) this._waterParticleScene.Instance();
 			waterParticleInstance.Position = this.GetViewport().GetMousePosition();
 			this.AddChild(waterParticleInstance);
 		}
 
+		//If the right mouse button is being pressed, creates a blocker from the start of the press to the end of the press
 		//TODO: draw blockers so that they are visible
 		if (Input.IsMouseButtonPressed(2)) {
-			if (this._currentPlacedBlocker == null) {
-				this._currentPlacedBlocker = new SegmentShape2D {
+			if (this._blockerSegmentBeingPlaced == null) {
+				this._blockerSegmentBeingPlaced = new SegmentShape2D {
 					A = this.GetViewport().GetMousePosition(),
 					B = this.GetViewport().GetMousePosition()
 				};
 				var body = new StaticBody2D();
 				body.AddChild(new CollisionShape2D {
-					Shape = this._currentPlacedBlocker
+					Shape = this._blockerSegmentBeingPlaced
 				});
 				this.AddChild(body);
 			}
 			else {
-				this._currentPlacedBlocker.B = this.GetViewport().GetMousePosition();
+				this._blockerSegmentBeingPlaced.B = this.GetViewport().GetMousePosition();
 			}
 		}
 		else {
-			this._currentPlacedBlocker = null;
+			this._blockerSegmentBeingPlaced = null;
 		}
-		
-		//KeyList.Escape
+
+		//Removes all children if the escape key is pressed
 		if (Input.IsKeyPressed(16777217)) {
 			foreach (var child in this.GetChildren()) {
-				this.RemoveChild((Node) child);		
+				this.RemoveChild((Node) child);
 			}
 		}
 	}
