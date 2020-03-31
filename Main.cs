58e1256f64eec21ec84a4c55784d734645e29262
SaurabhTotey <SaurabhTotey@gmail.com>
2020-03-31 13:42:44 -0600

    Made simple but faulty water attraction

--- Main.cs
+++ Main.cs
@@ -12,6 +12,9 @@ public class Main : Control {
 	//The user can place 'blockers', which are static segments that interact with the water; this is the blocker that is currently being placed
 	private SegmentShape2D _blockerSegmentBeingPlaced;
 
+	//A list of all water particles so that they can exert forces on each other
+	private readonly List<RigidBody2D> _allWaterParticles = new List<RigidBody2D>();
+	
 	//A list of all blockers so that they can be drawn
 	private readonly List<SegmentShape2D> _allBlockers = new List<SegmentShape2D>();
 
@@ -23,6 +26,32 @@ public class Main : Control {
 	}
 
 	/**
+	 * Handles attraction between water particles
+	 * Currently is inefficient
+	 */
+	public override void _PhysicsProcess(float delta) {
+		foreach (var p1 in this._allWaterParticles) {
+			foreach (var p2 in this._allWaterParticles) {
+				if (p2 == p1) { //TODO: also continue if the particles cannot see each other
+					continue;
+				}
+
+				var positionDifference = p1.Position - p2.Position;
+				var directionVector = positionDifference.Normalized();
+				var squaredDistance = positionDifference.LengthSquared();
+				p2.AddCentralForce(2 * directionVector / squaredDistance); //TODO: make this force repulsive if p1 is in a cone above p2
+			}
+		}
+	}
+
+	/**
+	 * Removes particles from the list of interacting particles if their center is out of bounds
+	 */
+	public override void _Process(float delta) {
+		this._allWaterParticles.RemoveAll(waterParticle => !this.GetViewportRect().HasPoint(waterParticle.Position));
+	}
+
+	/**
 	 * Handles inputs for the simulation
 	 */
 	public override void _Input(InputEvent inputEvent) {
@@ -31,6 +60,7 @@ public class Main : Control {
 			var waterParticleInstance = (Node2D) this._waterParticleScene.Instance();
 			waterParticleInstance.Position = this.GetViewport().GetMousePosition();
 			this.AddChild(waterParticleInstance);
+			this._allWaterParticles.Add((RigidBody2D) waterParticleInstance);
 		}
 
 		//If the right mouse button is being pressed, creates a blocker from the start of the press to the end of the press
@@ -63,6 +93,7 @@ public class Main : Control {
 			foreach (var child in this.GetChildren()) {
 				this.RemoveChild((Node) child);
 			}
+			this._allWaterParticles.Clear();
 			this._allBlockers.Clear();
 			this.Update();
 		}
